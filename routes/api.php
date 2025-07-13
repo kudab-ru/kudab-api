fc77cbe06e629b9b31b39bd3cb2d9261c00b6aa9
Solomka-0 <krasjuk2@gmail.com>
2025-07-13 15:12:10 +0300

    feature(infra): Базовая инфраструктура

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('users', \App\Http\Controllers\Controller::class);
+Route::get('/api/', function () { return response()->json(['ok' => true]); });
