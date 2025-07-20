15cbdb4fb3105571b571a6bd07b8706a20696db3
Solomka-0 <krasjuk2@gmail.com>
2025-07-20 13:05:09 +0300

    тест rollback

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
-    return response()->json(['ok' => true]);
+    return response()->json([
+        'ok' => true,
+        'result' => 'pong',
+    ]);
 });
