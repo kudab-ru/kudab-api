1f657a4f570005ba00deb425af831e7a4d37cf31
Solomka-0 <krasjuk2@gmail.com>
2026-05-07 22:00:10 +0300

    feature(make): parser-flags target + bump kudab-parser до cross-dedup 3-4

--- database/seeders/CommunitiesTableSeeder.php
+++ database/seeders/CommunitiesTableSeeder.php
@@ -222,6 +222,72 @@ http://nikitincenter.ru',
             ],
         ];
 
+        // DEV fixture из prod-like активных источников Воронежа.
+        // Нужны для полноценного reindex/smoke на 300+ постов и проверки guards.
+        $prodLikeCommunities = [
+            [65, 'Культурный регион Воронеж', '227249561', 'https://vk.com/culturalregion36'],
+            [54, 'ЗАповедныйВОРОНЕЖ', '204682632', 'https://vk.com/zavrn_guide'],
+            [13, 'Министерство культуры Воронежской области', '131045020', 'https://vk.com/kulturavo'],
+            [7, 'Воронежский государственный театр оперы и балета', '2375140', 'https://vk.com/operavrn'],
+            [84, 'Центр Креативных Индустрий «Матрёшка» | Воронеж', '214264347', 'https://vk.com/creativecentre_vrn'],
+            [12, 'Правительство Воронежской области', '182585808', 'https://vk.com/govvrn36'],
+            [79, 'Smart - парк "Дельфин"', '195572091', 'https://vk.com/dolphinvoronezh'],
+            [37, 'Воронежский областной краеведческий музей', '135394155', 'https://vk.com/kraeved_muz'],
+            [106, 'ВИНЗАВОД', '170377932', 'https://vk.com/vinzavodpro'],
+            [28, 'Отражение | Медиа ВГПУ', '39342464', 'https://vk.com/otrazhenie_vspu'],
+            [64, 'Центр развития творчества детей и юношества', '211864594', 'https://vk.com/center_tvorchestva_vrn'],
+            [70, 'Сити-парк «Град»', '18349255', 'https://vk.com/cityparkgrad'],
+            [15, 'VRN Guide', '47196432', 'https://vk.com/vrn.guide'],
+            [52, 'Театр на подушках | Воронеж | 0+', '209530726', 'https://vk.com/na_podushkah_teatr'],
+            [8, 'Театр кукол имени В.А. Вольховского', '23380395', 'https://vk.com/teatrkukolshut'],
+            [68, 'Платоновский фестиваль искусств', '24834832', 'https://vk.com/platonov_festival'],
+            [85, 'Загородный комплекс «Farm&Village»', '209261900', 'https://vk.com/farmvillage'],
+            [113, 'Рестобар "Pinta Haus" Воронеж', '98545917', 'https://vk.com/pinta_haus'],
+            [77, 'ТИЦ Воронежа', '147889146', 'https://vk.com/tic_vrn'],
+            [39, 'Музей имени Ивана Саввича Никитина', '134247073', 'https://vk.com/museum_of_nikitin'],
+            [76, 'Visit Voronezh | Визит Воронеж', '82760279', 'https://vk.com/visit_voronezh'],
+            [44, 'Анимационный фестиваль «МультПрактика» Воронеж', '219765673', 'https://vk.com/animfestvrn'],
+            [17, 'Анимационная студия Воронеж / Wizart Animation', '35460437', 'https://vk.com/voronezhanimationstudio'],
+            [123, 'ИЦАЭ Воронежа', '26889679', 'https://vk.com/atomvrn'],
+            [11, 'Театр Кот | Воронеж', '31211354', 'https://vk.com/kottheatre'],
+            [67, 'BIG BAND Воронежской Филармонии', '129210675', 'https://vk.com/club129210675'],
+            [47, 'Воронежский центр народного творчества и кино', '183434266', 'https://vk.com/vrnfolklor'],
+        ];
+
+        foreach ($prodLikeCommunities as [$id, $name, $externalId, $url]) {
+            $communities[] = [
+                'id' => $id,
+                'name' => $name,
+                'description' => $name,
+                'city' => 'Воронеж',
+                'street' => null,
+                'house' => null,
+                'avatar_url' => null,
+                'image_url' => null,
+                'last_checked_at' => null,
+                'verification_status' => 'approved',
+                'is_verified' => true,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+
+            $communitySocialLinks[] = [
+                'community_id' => $id,
+                'social_network_id' => 1,
+                'external_community_id' => $externalId,
+                'url' => $url,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+        }
+
+        // Проставляем city_id повторно после добавления prod-like communities.
+        foreach ($communities as &$community) {
+            $cityName = $community['city'] ?? null;
+            $community['city_id'] = $cityName ? ($cityIdByName[$cityName] ?? null) : null;
+        }
+        unset($community);
+
         foreach ($communities as $community) {
             DB::table('communities')->updateOrInsert(
                 ['id' => $community['id']],
